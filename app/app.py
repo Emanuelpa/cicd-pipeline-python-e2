f19c64acb081bbd24f0a0d64f04182914b4e1a0f
Emanuelpa <epatinov@eafit.edu.co>
2026-04-17 19:51:40 -0500

    Implement CI pipeline with Docker build and push

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
-def index():
+def index_get():
     """
     DOCSTRING
     """
@@ -18,35 +18,34 @@ def index():
 
 
 @app.route("/", methods=["POST"])
-def index():
+def index_post():
     """
     DOCSTRING
     """
     resultado = None
-    if request.method == "POST":
-        try:
-            num1 = float(request.form["num1"])
-            num2 = float(request.form["num2"])
-            operacion = request.form["operacion"]
+    try:
+        num1 = float(request.form["num1"])
+        num2 = float(request.form["num2"])
+        operacion = request.form["operacion"]
 
-            if operacion == "sumar":
-                resultado = sumar(num1, num2)
-            elif operacion == "restar":
-                resultado = restar(num1, num2)
-            elif operacion == "multiplicar":
-                resultado = multiplicar(num1, num2)
-            elif operacion == "dividir":
-                resultado = dividir(num1, num2)
-            else:
-                resultado = "Operación no válida"
-        except ValueError:
-            resultado = "Error: Introduce números válidos"
-        except ZeroDivisionError:
-            resultado = "Error: No se puede dividir por cero"
+        if operacion == "sumar":
+            resultado = sumar(num1, num2)
+        elif operacion == "restar":
+            resultado = restar(num1, num2)
+        elif operacion == "multiplicar":
+            resultado = multiplicar(num1, num2)
+        elif operacion == "dividir":
+            resultado = dividir(num1, num2)
+        else:
+            resultado = "Operación no válida"
+    except ValueError:
+        resultado = "Error: Introduce números válidos"
+    except ZeroDivisionError:
+        resultado = "Error: No se puede dividir por cero"
 
     return render_template("index.html", resultado=resultado)
 
 
 if __name__ == "__main__":  # pragma: no cover
     # Quita debug=True para producción
-    app.run(debug=False, port=5000, host="[IP_ADDRESS]")
+    app.run(debug=False, port=5000, host="127.0.0.1")
